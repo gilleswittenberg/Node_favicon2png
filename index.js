153b0c117fa6bbbba86695df979a4ab9001aa5c0
Gilles Wittenberg <git@gilleswittenberg.com>
2013-05-14 23:14:42 +0200

    Init

---
+++ index.js
@@ -0,0 +1,84 @@
+// requires
+var fs = require('fs');
+var path = require('path');
+var favicon = require('favicon');
+var wget = require('wget');
+var imagemagick = require('imagemagick');
+
+// settings
+var cachedDir = 'cached_images';
+var defaultPNG = 'favicon.png';
+
+// read setting
+function getSetting (setting) {
+  switch (setting) {
+    case 'cachedDir':
+      return cachedDir;
+    case 'defaultPNG':
+      return defaultPNG;
+  }
+}
+
+// configure
+function configure (config) {
+  Object.keys(config).forEach(function (o) {
+    switch (o) {
+      case 'cachedDir':
+        cachedDir = config[o];
+        break;
+      case 'defaultPNG':
+        defaultPNG = config[o];
+        break;
+    }
+  });
+  return this;
+}
+
+var showDefault = function showDefault (callback) {
+  fs.readFile(__dirname + '/' + defaultPNG, callback);
+};
+
+var readFile = function readFile (domainString, callback) {
+  var filename = __dirname + '/' + cachedDir + '/' + domainString + '.png';
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      favicon('http://' + domainString, function (err, faviconUrl) {
+        // show default
+        if (!faviconUrl) {
+          return showDefault(callback);
+        }
+        var tmpFilename = __dirname + '/tmp/' + domainString + path.extname(faviconUrl);
+        var download = wget.download(faviconUrl, tmpFilename);
+        // show default
+        download.on('error', function (err) {
+          showDefault(callback);
+        });
+        download.on('end', function () {
+          imagemagick.convert(
+            [tmpFilename, '-thumbnail', '16x16', '-flatten', filename],
+            function (err, stdout){
+              // remove tmp file
+              process.nextTick(function () {
+                fs.unlink(tmpFilename);
+              });
+              // show default
+              if (err) {
+                return showDefault(callback);
+              }
+              // show converted file
+              fs.readFile(filename, callback);
+            }
+          );
+        });
+      });
+    } else {
+      // show cached file
+      fs.readFile(filename, callback);
+    }
+  });
+};
+
+// exports
+exports.getSetting = getSetting;
+exports.configure = configure;
+exports.readFile = readFile;
---
+++ test/index.test.js
@@ -0,0 +1,83 @@
+var should  = require('should');
+var fs  = require('fs');
+var favicon2Image;
+
+// start with a clean module
+beforeEach(function () {
+  delete require.cache[require.resolve(__dirname + '/../')]
+  favicon2Image = require(__dirname + '/../');
+})
+
+describe("getSetting default cachedDir", function () {
+  it("get cachedDir", function (done) {
+    favicon2Image.getSetting('cachedDir').should.eql('cached_images');
+    done();
+  });
+  it("get defaultPNG", function (done) {
+    favicon2Image.getSetting('defaultPNG').should.eql('favicon.png');
+    done();
+  });
+});
+
+describe("configure", function () {
+  it("sets cachedDir using configure method", function (done) {
+    favicon2Image.configure({cachedDir: 'dir_name', defaultPNG: 'default.png'});
+    favicon2Image.getSetting('cachedDir').should.eql('dir_name');
+    favicon2Image.getSetting('defaultPNG').should.eql('default.png');
+    done();
+  });
+});
+
+describe("invalid domain", function () {
+  it("return default PNG", function (done) {
+    favicon2Image.readFile('invalid-domain', function (err, img) {
+      fs.readFile(__dirname + '/../favicon.png', function (errExpected, imgExpected) {
+        img.should.eql(imgExpected);
+        done();
+      });
+    });
+  });
+});
+
+describe("nonexisting domain/favicon", function () {
+  it("return default PNG", function (done) {
+    favicon2Image.readFile('www.nonexistingabcdefghijklmnopqrstuvwxyz', function (err, img) {
+      fs.readFile(__dirname + '/../favicon.png', function (errExpected, imgExpected) {
+        img.should.eql(imgExpected);
+        done();
+      });
+    });
+  });
+});
+
+describe("convert on non cached icon", function () {
+  it("not return default PNG", function (done) {
+    // remove cached file
+    var path = __dirname + '/../cached_images/www.github.com.png';
+    if (fs.existsSync(path)) {
+      fs.unlinkSync(path);
+    }
+    // check if converted file is returned
+    favicon2Image.readFile('www.github.com', function (err, img) {
+      fs.readFile(__dirname + '/../favicon.png', function (errExpected, imgExpected) {
+        img.should.not.eql(imgExpected);
+        done();
+      });
+    });
+  });
+});
+
+describe("show cached icon", function () {
+  it("not return default PNG", function (done) {
+    // check if cached file exists
+    var path = __dirname + '/../cached_images/www.github.com.png';
+    fs.existsSync(path).should.be.true;
+    // check if cached file is returned
+    favicon2Image.readFile('www.github.com', function (err, img) {
+      fs.readFile(path, function (errExpected, imgExpected) {
+        img.should.eql(imgExpected);
+        done();
+      });
+    });
+  });
+});
